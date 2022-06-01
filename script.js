86592f0af56ef80f49fb2ed13598177e762b6e11
gerlangiosoares <gerlangio@gmail.com>
2022-06-01 11:02:07 -0300

    atual

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+/*alert('Olá!') - é só um teste*/
+/**
+ * Passo 1 - criar uma funcion carregar
+ */
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+}
+/**
+ * Após criar essa função, vai no HTML no body e inicializa a function carregar()
+ */
